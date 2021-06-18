28c58c64f69800e23e5c4424eaba065193e26a76
Naomi Cespedes <lizcespedes0326@gmail.com>
2021-06-18 13:57:52 -0400

    Applying some changes to work better with the template

--- portfolioApp/src/app/app.module.ts
+++ portfolioApp/src/app/app.module.ts
@@ -3,29 +3,22 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { HeaderComponent } from './shared/header/header.component';
-import { FooterComponent } from './shared/footer/footer.component';
 import { PortfolioComponent } from './main/portfolio/portfolio.component';
 import { EducationComponent } from './main/education/education.component';
 import { AboutMeComponent } from './main/about-me/about-me.component';
-import { ContactComponent } from './shared/contact/contact.component';
-import { LinkedinComponent } from './shared/linkedin/linkedin.component';
-import { BannerComponent } from './shared/banner/banner.component';
+
+import { SharedModule } from './shared/shared.module';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HeaderComponent,
-    FooterComponent,
     PortfolioComponent,
     EducationComponent,
-    AboutMeComponent,
-    ContactComponent,
-    LinkedinComponent,
-    BannerComponent
+    AboutMeComponent
   ],
   imports: [
     BrowserModule,
+    SharedModule,
     AppRoutingModule
   ],
   providers: [],
