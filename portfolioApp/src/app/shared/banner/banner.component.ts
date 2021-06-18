3466a60f2b6169035ba25a29e2e9237f54edea0a
Naomi Cespedes <lizcespedes0326@gmail.com>
2021-06-18 00:52:56 -0400

    Creating my portfolio app's components

---
+++ portfolioApp/src/app/shared/banner/banner.component.ts
@@ -0,0 +1,15 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-banner',
+  templateUrl: './banner.component.html',
+  styles: [
+  ]
+})
+export class BannerComponent {
+
+  constructor() { }
+
+  
+
+}
