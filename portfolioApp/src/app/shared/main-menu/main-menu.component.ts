7f368f69ccbb8b395a9401a4a9b4fc17a121e46a
Naomi Cespedes <lizcespedes0326@gmail.com>
2021-07-23 22:45:51 -0400

    Adding Main-Menu to shared module

---
+++ portfolioApp/src/app/shared/main-menu/main-menu.component.ts
@@ -0,0 +1,16 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-main-menu',
+  templateUrl: './main-menu.component.html',
+  styles: [
+  ]
+})
+export class MainMenuComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
--- portfolioApp/src/app/shared/shared.module.ts
+++ portfolioApp/src/app/shared/shared.module.ts
@@ -2,13 +2,13 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { HeaderComponent } from './header/header.component';
 import { LoadingAnimationComponent } from './loading-animation/loading-animation.component';
-
-
+import { MainMenuComponent } from './main-menu/main-menu.component';
 
 @NgModule({
   declarations: [
     HeaderComponent,
-    LoadingAnimationComponent
+    LoadingAnimationComponent,
+    MainMenuComponent
 
   ],
   exports: [
