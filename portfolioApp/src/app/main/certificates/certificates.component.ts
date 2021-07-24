2a2b537f3b28052b302ca55faca885d89ee342fa
Naomi Cespedes <lizcespedes0326@gmail.com>
2021-07-24 00:00:37 -0400

    Creating certificates components

---
+++ portfolioApp/src/app/main/certificates/certificates.component.ts
@@ -0,0 +1,16 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-certificates',
+  templateUrl: './certificates.component.html',
+  styles: [
+  ]
+})
+export class CertificatesComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
