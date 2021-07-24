59c210e7cd93644402ac3f00020dceec189a659d
Naomi Cespedes <lizcespedes0326@gmail.com>
2021-07-23 23:18:40 -0400

    Creating knowledge and education component

---
+++ portfolioApp/src/app/main/knowledge/knowledge.component.ts
@@ -0,0 +1,16 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-knowledge',
+  templateUrl: './knowledge.component.html',
+  styles: [
+  ]
+})
+export class KnowledgeComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
